5411103b70159f57dd1d642fd3501b6df7a54df7
maruppi76 <maruppi76.y7a6@gmail.com>
2021-02-04 14:28:24 +0900

    [update] Userモデルに対するFactoryBotのファイル記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :user do
-    
+    first_name         { '山田' }
+    last_name          { '太郎' }
+    first_name_kana    { 'ヤマダ' }
+    last_name_kana     { 'タロウ' }
+    email              { 'taro.yamada@shifton.co.jp' }
+    password           { '1a' + Faker::Internet.password(min_length: 4, max_length: 18) }
+    staff_code         { 'A12345' }
+    status             { 1 }
+    role               { 1 }
+    admin              { true }
+    department_main_id { 1 }
+    department_sub_id  { 1 }
+
+    association :company
   end
 end
