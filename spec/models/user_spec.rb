6a5247a49cc91320a6f5e11103f17b15bb628533
maruppi76 <maruppi76.y7a6@gmail.com>
2021-02-04 17:50:10 +0900

    [add] Userモデル_正常系テスト記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    FactoryBot.build(:department)
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規作成' do
+    context '正常' do
+      it '全て正常' do
+        expect(@user.valid?).to eq true
+      end
+
+      it 'staff_code:任意' do
+        @user.staff_code = nil
+        expect(@user.valid?).to eq true
+      end
+
+      it 'department_sub_id:任意' do
+        @user.department_sub_id = nil
+        expect(@user.valid?).to eq true
+      end
+    end
+  end
 end
