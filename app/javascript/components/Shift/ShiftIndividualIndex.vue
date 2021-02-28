5a154fc74ddadf6f378b04c4fc82e30f710f14ab
maruppi76 <maruppi.y7a6@gmail.com>
2021-02-28 23:45:19 +0900

    [add]ヘッダー部分の作成

--- app/javascript/components/Shift/ShiftIndividualIndex.vue
+++ app/javascript/components/Shift/ShiftIndividualIndex.vue
@@ -3,10 +3,26 @@
     <v-card elevation="0" class="rounded-0">
       <v-card-title
         class="blue-grey lighten-5"
-      >確定シフト - {{ current_user.first_name }}  {{ current_user.last_name }}さん</v-card-title>
+      >確定シフト</v-card-title>
     </v-card>
-     <v-card elevation="2" class="rounded-0 pa-10 ma-2">
-     </v-card>
+    <v-card elevation="2" class="rounded-0 ma-2">
+      <v-row class="pa-3" justify="space-between">
+        <v-col cols="auto" class="text-h5">
+          {{ current_user.first_name }}  {{ current_user.last_name }}
+        </v-col>
+        <v-col cols="auto">
+          <v-row>
+            <v-select
+              label="Standard"
+              class="mr-1"
+            ></v-select>
+            <v-select
+              label="Standard"
+            ></v-select>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-card>
   </v-app>
 </template>
 
