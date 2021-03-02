db3e513c97ab30c8d89efb39f6586b44d7c2996c
maruppi76 <maruppi.y7a6@gmail.com>
2021-03-02 13:33:24 +0900

    [add]表示ボタンの追加

--- app/javascript/components/Shift/ShiftIndividualIndex.vue
+++ app/javascript/components/Shift/ShiftIndividualIndex.vue
@@ -22,6 +22,10 @@
               class="mr-2"
               :items="monthes"
             ></v-select>
+            <v-btn
+              outlined
+              class="mr-2"
+            >表示</v-btn>
           </v-row>
         </v-col>
       </v-row>
