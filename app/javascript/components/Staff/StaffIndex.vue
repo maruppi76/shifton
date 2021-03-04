273a18a4e2b65bce18f0eccba151ceda4d11aa29
maruppi76 <maruppi.y7a6@gmail.com>
2021-03-04 15:46:29 +0900

    [add]スタッフ一覧表示のロジック

--- app/javascript/components/Staff/StaffIndex.vue
+++ app/javascript/components/Staff/StaffIndex.vue
@@ -5,5 +5,23 @@
         class="blue-grey lighten-5"
       >スタッフ一覧</v-card-title>
     </v-card>
+    <v-row class="ma-0 align-content-start">
+      <v-col cols="1" v-for="n in 20" :key="n">
+        <v-card
+          elevation="0"
+          tile
+        >
+          <v-img
+            class="rounded-circle"
+            :aspect-ratio="1/1"
+            width="200px"
+            src="https://picsum.photos/id/11/500/300"
+          ></v-img>
+          <div class="text-center my-3">
+            山田 太郎
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-app>
 </template>
