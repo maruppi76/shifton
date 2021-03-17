e32c4346e70cb1eaada6989547116f2ee14a0a7a
maruppi76 <maruppi.y7a6@gmail.com>
2021-03-17 11:17:42 +0900

    [add]Header部分の作成

---
+++ app/javascript/components/Staff/StaffDetail.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app>
+    <v-card elevation="0" class="rounded-0">
+      <v-card-title
+        class="blue-grey lighten-5"
+      >スタッフ名</v-card-title>
+    </v-card>
+  </v-app>
+</template>
