00e093c26a988d685f6d2e5de9c5976ec7746fdf
maruppi76 <maruppi.y7a6@gmail.com>
2021-03-26 14:47:52 +0900

    [add]ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "home#index"
 
   namespace :api, {format: 'json'} do
-    resources :users, only:[:show] do
+    resources :users, only:[:show, :update] do
       collection do
         get "all_users"
         get "login_check"
