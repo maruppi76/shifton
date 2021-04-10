272e311009fc6c036845b238f622d743c233a451
maruppi76 <maruppi.y7a6@gmail.com>
2021-04-10 12:49:03 +0900

    [add]Main部分の枠組み作成

--- app/javascript/components/Shift/ShiftDepartmentIndex.vue
+++ app/javascript/components/Shift/ShiftDepartmentIndex.vue
@@ -5,5 +5,12 @@
         class="blue-grey lighten-5"
       >確定シフト</v-card-title>
     </v-card>
+    <v-card elevation="2" class="rounded-0 ma-1 ma-lg-4">
+      <v-row class="pa-3" justify="space-between">
+        <v-col cols="auto" class="text-h5">
+          
+        </v-col>
+      </v-row>
+    </v-card>
   </v-app>
 </template>
