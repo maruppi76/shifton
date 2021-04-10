392bf78a12e53975af72d4acc80d2ba3924786c8
maruppi76 <maruppi.y7a6@gmail.com>
2021-04-10 12:56:56 +0900

    [add]部署名を表示

--- app/javascript/components/Shift/ShiftDepartmentIndex.vue
+++ app/javascript/components/Shift/ShiftDepartmentIndex.vue
@@ -6,11 +6,29 @@
       >確定シフト</v-card-title>
     </v-card>
     <v-card elevation="2" class="rounded-0 ma-1 ma-lg-4">
-      <v-row class="pa-3" justify="space-between">
+      <v-row class="pa-3" justify="space-between" v-if="current_user">
         <v-col cols="auto" class="text-h5">
-          
+          {{ current_user.department_main.mobile }}
         </v-col>
       </v-row>
     </v-card>
   </v-app>
 </template>
+
+<script>
+  import axios from 'axios';
+  export default {
+    data() {
+      return {
+        current_user: '',
+      }
+    },
+    mounted() {
+      axios.get('/api/users/user_detail.json')
+        .then(response => {
+          this.current_user = response.data
+        })
+        .catch(error => console.log(error))
+    }
+  }
+</script>
