1a42f2c7c7068ae296362ccd1fb533dd9832dad8
maruppi76 <maruppi.y7a6@gmail.com>
2021-04-10 12:44:36 +0900

    [add]Header部分の記述

---
+++ app/javascript/components/Shift/ShiftDepartmentIndex.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app>
+    <v-card elevation="0" class="rounded-0">
+      <v-card-title
+        class="blue-grey lighten-5"
+      >確定シフト</v-card-title>
+    </v-card>
+  </v-app>
+</template>
